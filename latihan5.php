e46a45aa8f3c0ab8d550e19caf02c6d574c23fa3
farhanz17 <92637117+farhanz17@users.noreply.github.com>
2023-03-21 09:37:07 +0700

    Add files via upload

---
+++ latihan5.php
@@ -0,0 +1,11 @@
+<h2>Php dasar</h2>
+<?php
+$nama_hari = date("l");
+if ($nama_hari == "Sunday") {
+echo "Minggu";
+} elseif ($nama_hari == "Monday") {
+echo "Senin";
+} else {
+echo "Selasa";
+}
+?>
---
+++ latihan8.php
@@ -0,0 +1,9 @@
+<h2>php dasar</h2>
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+while ($i<=10) {
+echo "Perulangan ke: " . $i . '<br />';
+$i++;
+}
+?>
